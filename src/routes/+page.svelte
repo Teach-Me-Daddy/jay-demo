46ce2338193db587a7c0edfa330758ae21e0050b
jaykasiphat <jaykasiphat@gmail.com>
2024-07-09 00:55:12 +0700

    Create main game page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1 class="bg-purple-200 text-xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svesDFSDFSDlte.dev</a> to read the documentation!</p>
+<script></script>
+
+<div class="p-8 flex justify-center items-center h-screen">
+  <div class="max-w-lg mx-0 p-12 border rounded-xl bg-slate-50">
+    <h1 class="text-3xl mb-2 font-extrabold">Click Me</h1>
+    <h2 class="font-thin mb-20 text-slate-500">Click to find out what your finger is capable of 👀</h2>
+
+    <div class="mb-20 text-center">
+      <p class="text-9xl mb-2">0</p>
+      <button class="btn rounded-xl bg-white">Click me</button>
+    </div>
+
+    <div class="controls text-center">
+      <p class="mb-4 text-xl">0:00</p>
+      <button class="btn border-blue-600 bg-blue-600 text-white mr-4">Start</button>
+      <button class="btn border-red-500 bg-red-100 text-rose-500">Stop</button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .btn {
+    @apply border rounded-lg py-2 px-12 font-medium;
+  }
+</style>
