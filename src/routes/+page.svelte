8e06bcba2b91d3be9d1ed042c254704564ae1c2b
jaykasiphat <jaykasiphat@gmail.com>
2024-07-08 12:54:18 +0700

    Changed background color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="bg-yellow-500">Welcome to SvelteKit</h1>
+<h1 class="bg-purple-200 text-xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svesDFSDFSDlte.dev</a> to read the documentation!</p>
