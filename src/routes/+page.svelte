76ac5fc6f0e808280b385dfe5f427c21bf5bcf98
jaykasiphat <jaykasiphat@gmail.com>
2024-07-08 12:25:42 +0700

    Add Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="bg-yellow-500">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svesDFSDFSDlte.dev</a> to read the documentation!</p>
