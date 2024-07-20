9db27d5ba4f70fe48435ac4c7310de820b393070
jaykasiphat <jaykasiphat@gmail.com>
2024-07-21 01:45:19 +0700

    Add animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,44 @@
-<script lang='ts'>
-  import { goto } from '$app/navigation';
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { fade, fly } from "svelte/transition";
 
   const navigateTo = (path: string) => {
     goto(`/${path}`);
   };
 </script>
 
-<div>
-  <div class="text-center flex flex-col items-center justify-center h-screen lg:flex-row lg:justify-center">
-    <div class="v1 card">
-      <h2 class="title">ClickMe</h2>
-      <p class="sub-title">Play the original click game to see how fast you can click!</p>
-      <button class="cursor-pointer btn" on:click={() => navigateTo('v1')}>Play ClickMe</button>
-    </div>
-    <div class="v2 card">
-      <h2 class="title">Click-a-Mole</h2>
-      <p class="sub-title">Challenge yourself by only clicking the mole!</p>
-      <button class="cursor-pointer btn" on:click={() => navigateTo('v2')}>Play Click-a-Mole</button>
-    </div>
+<div
+  transition:fade
+  class="text-center flex flex-col items-center justify-center h-screen lg:flex-row lg:justify-center"
+>
+  <div in:fly={{ x: "-10em" }} class="v1 card">
+    <h2 class="title">ClickMe</h2>
+    <p class="sub-title">
+      Play the original click game to see how fast you can click!
+    </p>
+    <button class="cursor-pointer btn" on:click={() => navigateTo("v1")}
+      >Play ClickMe</button
+    >
+  </div>
+  <div in:fly={{ x: "10em" }} class="v2 card">
+    <h2 class="title">Click-a-Mole</h2>
+    <p class="sub-title">Challenge yourself by only clicking the mole!</p>
+    <button class="cursor-pointer btn" on:click={() => navigateTo("v2")}
+      >Play Click-a-Mole</button
+    >
   </div>
 </div>
 
-<style lang='postcss'>
+<style lang="postcss">
   .btn {
-    @apply
-      border rounded-full
+    @apply border rounded-full
       py-2 px-3
       font-bold text-white
       bg-blue-600;
   }
 
   .card {
-    @apply
-      flex flex-col justify-between
+    @apply flex flex-col justify-between
       border border-blue-700 rounded-lg
       py-10 px-10 h-1/3 m-10
       w-8/12 lg:w-4/12
